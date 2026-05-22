6e93d056eac8448b69d9b3dc95789fa5a9349a6a
Lana <koalanako@gmail.com>
2026-05-22 15:52:01 -0700

    add projects component

---
+++ src/components/Projects.jsx
@@ -0,0 +1,10 @@
+const Projects = () => {
+  return (
+    <section id="projects" className="flex-row justify-center mb-[2rem]
+    items-center py-1 px-1 lg:px-5 relative z-50 bg-[#0d1219]">
+
+    </section>
+  )
+}
+
+export default Project
