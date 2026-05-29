dab3d1b6ad1f6d602225d55dd7b845c16fb7c088
Lana <koalanako@gmail.com>
2026-05-29 15:16:34 -0700

    update projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,5 +5,12 @@ export const projects = [
     description: "A volunteer organizer for non-profits. 1st place award in the beginner stream at UBC WiCS's YouCode Hackathon in April 2026.",
     tech: ["HTML", "CSS", "JavaScript", "Supabase"],
     live: "https://vivpeng.github.io/youcode-2026/",
+  },
+  {
+    id: 2,
+    title: "CARDS AGAINST BOREDOM (in progress)",
+    description: "CPSC 210 Term Project—built using Java with a focus on OOP, software design principles, and TDD—extended to a React web app using Spring Boot.",
+    tech: ["Java", "React", "Tailwind CSS v3", "JavaScript", "Spring Boot"],
+    live: "https://github.com/nakailana/project-cardgame",
   }
 ]
